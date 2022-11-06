6ab612299c2645d527065b21dc60d390c6ecd33f
EliteAsian <lavasnakegaming@gmail.com>
2022-11-06 17:20:22 -0500

    Number literal seperators

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { posix } from 'path';
+import { posix } from "path";
 
 export async function activate(context: vscode.ExtensionContext) {
 	context.workspaceState.update("scopeXml", null);
